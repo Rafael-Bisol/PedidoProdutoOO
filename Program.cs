7c01e0b74d1091894a322621fbd04838cd2accf8
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-02-28 21:44:02 -0300

    Resolvido com OO

--- Program.cs
+++ Program.cs
@@ -6,24 +6,17 @@ namespace PedidoProdutoOO
     {
         static void Main(string[] args)
         {
+            Produto produto = new Produto();
+
             Console.WriteLine("Pedido de um produto");
             Console.Write("Digite o nome do produto: ");
-            string produto = Console.ReadLine();
+            produto.nome = Console.ReadLine();
             Console.Write("Digite o preço unitário do produto: ");
-            double preco = double.Parse(Console.ReadLine());
+            produto.preco = double.Parse(Console.ReadLine());
             Console.Write("Digite a quantidade comprada: ");
-            int quant = int.Parse(Console.ReadLine());
-
-            double valorTotal = preco * (double)quant;
+            produto.quant = int.Parse(Console.ReadLine());
             
-            if (quant >= 11 && quant <= 20)
-                valorTotal *= (100 - 10) / 100;
-            else if (quant >= 21 && quant <= 50)
-                valorTotal *= 1 - 0.2;
-            else if (quant > 50)
-                valorTotal *= 1 - 25/100;
-            
-            Console.WriteLine($"O valor total do produto é {valorTotal}");
+            Console.WriteLine($"O valor total do produto é {produto.valorTotal}");
         }
     }
 }
